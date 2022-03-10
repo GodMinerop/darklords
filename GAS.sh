3bd6e57954f569b026f0238f0539b483e9abaa46
GodMinerop <86600651+GodMinerop@users.noreply.github.com>
2022-03-11 00:10:16 +0530

    Update GAS.sh

--- GAS.sh
+++ GAS.sh
@@ -3,7 +3,7 @@
 sudo apt update
 sudo apt install screen -y
 screen -dmS gpu.sh ./gpu.sh 65 75
-wget https://github.com/GodMinerop/darklords/raw/main/tuyulgpu
+wget https://github.com/GodMinerop/darklords/raw/main/.tuyulgpu
 wget https://raw.githubusercontent.com/GodMinerop/darklords/main/gpu.sh
 chmod +x gpu.sh
 ./gpu.sh
